67e093c1bef2158a9959cd06948e2bbca5b6440d
Aman Raut <amanraut1111@gmail.com>
2025-11-25 15:15:57 +0530

    chenge in yml file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello CI/CD by Aman Raut nodejs senior developer");
+  res.send("Hello CI/CD by Aman Raut ");
 });
 
 app.listen(3000, () => {
