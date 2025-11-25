2f3e53346b6e91b9389483121235952d777a310c
Aman Raut <amanraut1111@gmail.com>
2025-11-25 20:57:40 +0530

    ad env variable

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello CI/CD by Aman Raut 11111111111 ");
+  res.send("Hello CI/CD by Aman Raut ");
 });
 
 app.listen(3000, () => {
