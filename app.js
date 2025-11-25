312688e6985dba26d25549adc656e619d72f86ac
Aman Raut <amanraut1111@gmail.com>
2025-11-25 18:29:48 +0530

    ad env variable

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+require("dotenv").config();
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello CI/CD by Aman Raut ");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("sever is listeming on port 3000");
 });
